ebe2c34b2686591f0494413a7bc3b6903df91813
hindu-muppala <hindumuppala@gmail.com>
2023-04-04 22:02:13 +0530

    added test

---
+++ converter_test.go
@@ -0,0 +1,20 @@
+package converter_test
+ 
+import (
+	"testing"
+	"fmt"
+	"os/exec"
+	//"log"
+	c "github.com/hindu-muppala/YamlJsonConverter"
+)
+
+func TestAdd(t *testing.T){
+	x:=c.convertJson("./cmd/testfiles/test2.yaml","./cmd/testfiles/test1.json")
+	y:=c.convertYaml("./cmd/testfiles/test.json","./cmd/testfiles/test3.yaml")
+	if x!=0 || y!=0 {
+		t.Errorf("eroor")
+	}else{
+		fmt.Print("conversion successful")
+	}
+
+}
